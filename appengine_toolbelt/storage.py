8b1b979fb030c268c6a8290ce8a8d76a2b289912
Rafael Braz <rafael@rbraz.net>
2017-10-18 12:02:47 -0200

    Ajustes no cloud storage

--- appengine_toolbelt/storage.py
+++ appengine_toolbelt/storage.py
@@ -1,4 +1,6 @@
 import cloudstorage
+import mimetypes
+import urllib
 from django.conf import settings
 from django.core.files.storage import Storage
 from django.core.files.base import File
@@ -8,36 +10,45 @@ class GoogleCloudStorage(Storage):
 
     def __init__(self, bucket=None):
         self.bucket = bucket or settings.DEFAULT_STORAGE_BUCKET
-        self.bucket = '/' + self.bucket + '/'
+
+    def _add_bucket(self, name):
+        safe_name = urllib.quote(name.encode('utf-8'))
+        return '/{0}/{1}'.format(self.bucket, safe_name)
 
     def _open(self, name, mode='r'):
+        # Handle 'rb' as 'r'.
         mode = mode[:1]
-        return File(cloudstorage.open(self.bucket + name, mode))
+        fp = cloudstorage.open(self._add_bucket(name), mode=mode)
+        return File(fp)
+
+    def _content_type_for_name(self, name):
+        # guess_type returns (None, encoding) if it can't guess.
+        return mimetypes.guess_type(name)[0] or DEFAULT_CONTENT_TYPE
 
     def _save(self, name, content):
-        gcs_file = None
-        for chunk in content.chunks():
-            if gcs_file is None:
-                gcs_file = cloudstorage.open(self.bucket + name, 'w')
-            gcs_file.write(chunk)
-        if gcs_file is not None:
-            gcs_file.close()
+        name = self.get_valid_name(name)  # Make sure the name is valid
+
+        kwargs = {
+            'content_type': self._content_type_for_name(name),
+        }
+        with cloudstorage.open(self._add_bucket(name), 'w', **kwargs) as fp:
+            fp.write(content.read())
         return name
 
     def delete(self, name):
         assert name, "The name argument is not allowed to be empty."
-        cloudstorage.delete(self.bucket + name)
+        cloudstorage.delete(self._add_bucket(name))
 
     def exists(self, name):
         try:
-            cloudstorage.stat(self.bucket + name)
+            cloudstorage.stat(self._add_bucket(name))
         except cloudstorage.NotFoundError as e:
             return False
         else:
             return True
 
     def size(self, name):
-        return cloudstorage.stat(self.bucket + name).st_size
+        return cloudstorage.stat(self._add_bucket(name)).st_size
 
     def url(self, name):
         return settings.MEDIA_URL + name
